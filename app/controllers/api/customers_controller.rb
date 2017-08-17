ee874fea2c1eacfe4d68f322d9568b765237f202
Menachem Abraham <menachemabraham@gmail.com>
2017-08-17 10:04:40 -0400

    Add customer create action

--- app/controllers/api/customers_controller.rb
+++ app/controllers/api/customers_controller.rb
@@ -4,6 +4,15 @@ class Api::CustomersController < ApplicationController
     render :index
   end
 
+  def create
+    @customer = Customer.new(customer_params)
+    if @customer.save
+      render json: @customer
+    else
+      render json: @customer.errors
+    end
+  end
+
   def customer_params
     params.require(:customer).permit(:name, :email, :phone_number, :address, :notes)
   end
