510c3b61f46a4c726eb1023469c094021667afdf
Menachem Abraham <menachemabraham@gmail.com>
2017-08-17 20:14:53 -0400

    Add byIds selector

--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -1,1 +1,3 @@
-export const asArray = obj => Object.keys(obj).map((key) => obj[key]);
+export const asArray = obj => Object.keys(obj).map(key => obj[key]);
+
+export const byIds = (entities, ids) => ids.map(id => entities[id]);
