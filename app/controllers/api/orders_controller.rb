b74704621a7b5074431f6c6b4a4a18fcb9c4fce3
Menachem Abraham <menachemabraham@gmail.com>
2017-08-30 11:45:30 -0400

    Enable updating empty order

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -34,7 +34,9 @@ class Api::OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
-    if @order.update(order_params)
+    updated_params = order_params
+    updated_params[:dress_ids] ||= []
+    if @order.update(updated_params)
       render json: @order.id
     else
       render json: @order.errors, status: 422
