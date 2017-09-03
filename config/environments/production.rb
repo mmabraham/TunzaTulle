37edfa59d357262d27b17d98d62644feb86ba55e
Menachem Abraham <menachemabraham@gmail.com>
2017-09-03 18:09:05 -0400

    Fix smtp domain settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,10 +82,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'TunzaTulle.com',
+    domain:               'gmail.com',
     user_name:            ENV['email_username'],
     password:             ENV['email_password'],
-    authentication:       'plain',
+    authentication:       'login',
     enable_starttls_auto: true
   }
 end
