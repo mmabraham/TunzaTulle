1c7b769e1a2ba1099249240c14135870ce6bb301
Menachem Abraham <menachemabraham@gmail.com>
2017-09-03 15:39:19 -0400

    Add link to dresses/new from nav menu

--- frontend/components/menu.jsx
+++ frontend/components/menu.jsx
@@ -3,18 +3,21 @@ import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import { NavLink } from 'react-router-dom';
 
 const Menu = props => {
   return (
     <IconMenu
-      {...props}
       iconButtonElement={
         <IconButton><MoreVertIcon /></IconButton>
       }
       targetOrigin={{horizontal: 'right', vertical: 'top'}}
       anchorOrigin={{horizontal: 'right', vertical: 'top'}}
       >
-      <MenuItem primaryText="Refresh" />
+      <MenuItem primaryText="Refresh" onTouchTap={() => window.location.reload()}/>
+      <NavLink to="/dresses/new" >
+        <MenuItem primaryText="List a new dress" />
+      </NavLink>
       <MenuItem primaryText="Help" />
       <MenuItem primaryText="Sign out" onTouchTap={props.logout} />
     </IconMenu>
