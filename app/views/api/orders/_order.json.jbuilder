250243003047d89a33931cb4f36f45077d860c2a
Menachem Abraham <menachemabraham@gmail.com>
2017-09-05 23:02:36 -0400

    Display customer name in order list

--- app/views/api/orders/_order.json.jbuilder
+++ app/views/api/orders/_order.json.jbuilder
@@ -1,3 +1,4 @@
 json.extract! order, :id, :event_date, :start_date, :end_date, :status
 json.dress_ids order.dress_orders, :dress_id
 json.customer_id order.customer.id
+json.customer_name order.customer.name
--- frontend/components/orders/order_list_item.jsx
+++ frontend/components/orders/order_list_item.jsx
@@ -7,8 +7,9 @@ const OrderListItem = props => {
   return (
     <NavLink to={`/orders/${props.order.id}`}>
       <MenuItem primaryText={props.order.status}>
-        <p>{moment.default(props.order.event_date).fromNow()} - 
-          {props.order && props.order.dress_ids.length} dresses
+        <p>{moment.default(props.order.event_date).fromNow()} -
+          {props.order && props.order.dress_ids.length} dresses - 
+          {props.order && props.order.customer_name}
         </p>
       </MenuItem>
     </NavLink>
