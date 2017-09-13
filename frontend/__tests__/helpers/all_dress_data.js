765a9f5cf978be7e712f44af7155ea8a349a242c
Menachem Abraham <menachemabraham@gmail.com>
2017-09-13 11:20:12 -0400

    Specify node version

---
+++ frontend/__tests__/helpers/all_dress_data.js
@@ -0,0 +1,544 @@
+const allDressData = [
+  {
+    "id": 37,
+    "barcode": "9466851613317",
+    "title": "Fantastic Bronze Computer",
+    "color": "pink",
+    "description": "Sustainable cred ethical authentic seitan. Kitsch hella blog truffaut bicycle rights kogi. Slow-carb fashion axe cleanse pickled vinyl readymade xoxo. Mustache jean shorts fanny pack.",
+    "waist": "48.8",
+    "min_waist": "47.3",
+    "max_waist": "50.3",
+    "sleeve_length": "Half Length",
+    "height": "46.2",
+    "min_height": "39.2",
+    "max_height": "48.2",
+    "age": 12,
+    "min_age": 10,
+    "max_age": 14,
+    "img": "https://s3.amazonaws.com/tunza-dev/dresses/images/000/000/037/original/18358754_1186415891467981_890714927206913499_o.jpg?1505155167",
+    "order_dates": [
+      {
+        "start_date": "2017-05-01T18:39:29.113Z",
+        "end_date": "2017-05-15T18:39:29.113Z",
+        "id": 15
+      },
+      {
+        "start_date": "2017-05-26T18:39:29.113Z",
+        "end_date": "2017-06-09T18:39:29.113Z",
+        "id": 40
+      },
+      {
+        "start_date": "2017-06-20T18:39:29.113Z",
+        "end_date": "2017-07-04T18:39:29.113Z",
+        "id": 65
+      },
+      {
+        "start_date": "2017-07-17T18:39:29.113Z",
+        "end_date": "2017-07-31T18:39:29.113Z",
+        "id": 92
+      },
+      {
+        "start_date": "2017-08-10T18:39:29.113Z",
+        "end_date": "2017-08-24T18:39:29.113Z",
+        "id": 116
+      },
+      {
+        "start_date": "2017-08-28T18:39:29.113Z",
+        "end_date": "2017-09-11T18:39:29.113Z",
+        "id": 134
+      },
+      {
+        "start_date": "2017-09-24T18:39:29.113Z",
+        "end_date": "2017-10-08T18:39:29.113Z",
+        "id": 161
+      },
+      {
+        "start_date": "2017-10-17T18:39:29.113Z",
+        "end_date": "2017-10-31T18:39:29.113Z",
+        "id": 184
+      }
+    ]
+  },
+  {
+    "id": 38,
+    "barcode": "6637426047148",
+    "title": "Awesome Paper Clock",
+    "color": "silver",
+    "description": "Venmo poutine meh semiotics. Kale chips plaid knausgaard tofu lomo slow-carb retro mixtape. Retro fingerstache yuccie tattooed cold-pressed.",
+    "waist": "31.8",
+    "min_waist": "30.3",
+    "max_waist": "33.3",
+    "sleeve_length": "long",
+    "height": "66.0",
+    "min_height": "59.0",
+    "max_height": "68.0",
+    "age": 12,
+    "min_age": 10,
+    "max_age": 14,
+    "img": "https://s3.amazonaws.com/tunza-dev/dresses/images/000/000/038/original/18359407_1187177241391846_2586655739208193281_o.jpg?1505155167",
+    "order_dates": [
+      {
+        "start_date": "2017-05-01T18:39:29.113Z",
+        "end_date": "2017-05-15T18:39:29.113Z",
+        "id": 15
+      },
+      {
+        "start_date": "2017-05-27T18:39:29.113Z",
+        "end_date": "2017-06-10T18:39:29.113Z",
+        "id": 41
+      },
+      {
+        "start_date": "2017-06-21T18:39:29.113Z",
+        "end_date": "2017-07-05T18:39:29.113Z",
+        "id": 66
+      },
+      {
+        "start_date": "2017-07-18T18:39:29.113Z",
+        "end_date": "2017-08-01T18:39:29.113Z",
+        "id": 93
+      },
+      {
+        "start_date": "2017-08-10T18:39:29.113Z",
+        "end_date": "2017-08-24T18:39:29.113Z",
+        "id": 116
+      },
+      {
+        "start_date": "2017-08-29T18:39:29.113Z",
+        "end_date": "2017-09-12T18:39:29.113Z",
+        "id": 135
+      },
+      {
+        "start_date": "2017-09-24T18:39:29.113Z",
+        "end_date": "2017-10-08T18:39:29.113Z",
+        "id": 161
+      },
+      {
+        "start_date": "2017-10-18T18:39:29.113Z",
+        "end_date": "2017-11-01T18:39:29.113Z",
+        "id": 185
+      }
+    ]
+  },
+  {
+    "id": 39,
+    "barcode": "3646403067690",
+    "title": "Rustic Granite Car",
+    "color": "silver",
+    "description": "Helvetica skateboard mustache direct trade. Cray crucifix squid. Austin truffaut blue bottle waistcoat tacos forage health. Fixie truffaut before they sold out. Tacos next level swag etsy plaid.",
+    "waist": "49.4",
+    "min_waist": "47.9",
+    "max_waist": "50.9",
+    "sleeve_length": "half length",
+    "height": "57.2",
+    "min_height": "50.2",
+    "max_height": "59.2",
+    "age": 9,
+    "min_age": 7,
+    "max_age": 11,
+    "img": "https://s3.amazonaws.com/tunza-dev/dresses/images/000/000/039/original/18401857_1189505594492344_6956178783894706389_o.jpg?1505155167",
+    "order_dates": [
+      {
+        "start_date": "2017-05-02T18:39:29.113Z",
+        "end_date": "2017-05-16T18:39:29.113Z",
+        "id": 16
+      },
+      {
+        "start_date": "2017-05-28T18:39:29.113Z",
+        "end_date": "2017-06-11T18:39:29.113Z",
+        "id": 42
+      },
+      {
+        "start_date": "2017-06-21T18:39:29.113Z",
+        "end_date": "2017-07-05T18:39:29.113Z",
+        "id": 66
+      },
+      {
+        "start_date": "2017-07-18T18:39:29.113Z",
+        "end_date": "2017-08-01T18:39:29.113Z",
+        "id": 93
+      },
+      {
+        "start_date": "2017-08-11T18:39:29.113Z",
+        "end_date": "2017-08-25T18:39:29.113Z",
+        "id": 117
+      },
+      {
+        "start_date": "2017-08-30T18:39:29.113Z",
+        "end_date": "2017-09-13T18:39:29.113Z",
+        "id": 136
+      },
+      {
+        "start_date": "2017-09-25T18:39:29.113Z",
+        "end_date": "2017-10-09T18:39:29.113Z",
+        "id": 162
+      },
+      {
+        "start_date": "2017-10-19T18:39:29.113Z",
+        "end_date": "2017-11-02T18:39:29.113Z",
+        "id": 186
+      }
+    ]
+  },
+  {
+    "id": 40,
+    "barcode": "2204210984244",
+    "title": "Heavy Duty Granite Bag",
+    "color": "sky blue",
+    "description": "Twee roof normcore wes anderson retro truffaut diy hella. Venmo ethical artisan cardigan listicle. Narwhal semiotics flannel celiac lo-fi intelligentsia. Jean shorts pop-up you probably haven't heard of them messenger bag 3 wolf moon.",
+    "waist": "30.6",
+    "min_waist": "29.1",
+    "max_waist": "32.1",
+    "sleeve_length": "Half Length",
+    "height": "65.0",
+    "min_height": "58.0",
+    "max_height": "67.0",
+    "age": 13,
+    "min_age": 11,
+    "max_age": 15,
+    "img": "https://s3.amazonaws.com/tunza-dev/dresses/images/000/000/040/original/18402215_1187197084723195_2275927402058387666_o.jpg?1505155167",
+    "order_dates": [
+      {
+        "start_date": "2017-05-03T18:39:29.113Z",
+        "end_date": "2017-05-17T18:39:29.113Z",
+        "id": 17
+      },
+      {
+        "start_date": "2017-05-29T18:39:29.113Z",
+        "end_date": "2017-06-12T18:39:29.113Z",
+        "id": 43
+      },
+      {
+        "start_date": "2017-06-21T18:39:29.113Z",
+        "end_date": "2017-07-05T18:39:29.113Z",
+        "id": 66
+      },
+      {
+        "start_date": "2017-07-19T18:39:29.113Z",
+        "end_date": "2017-08-02T18:39:29.113Z",
+        "id": 94
+      },
+      {
+        "start_date": "2017-08-12T18:39:29.113Z",
+        "end_date": "2017-08-26T18:39:29.113Z",
+        "id": 118
+      },
+      {
+        "start_date": "2017-08-30T18:39:29.113Z",
+        "end_date": "2017-09-13T18:39:29.113Z",
+        "id": 136
+      },
+      {
+        "start_date": "2017-09-25T18:39:29.113Z",
+        "end_date": "2017-10-09T18:39:29.113Z",
+        "id": 162
+      },
+      {
+        "start_date": "2017-10-20T18:39:29.113Z",
+        "end_date": "2017-11-03T18:39:29.113Z",
+        "id": 187
+      }
+    ]
+  },
+  {
+    "id": 41,
+    "barcode": "0034473154526",
+    "title": "Mediocre Concrete Plate",
+    "color": "mint green",
+    "description": "Microdosing street cliche chia meditation. Biodiesel bushwick meggings cleanse roof try-hard. Heirloom retro chambray. Brooklyn sartorial 3 wolf moon austin.",
+    "waist": "31.9",
+    "min_waist": "30.4",
+    "max_waist": "33.4",
+    "sleeve_length": "3 / 4",
+    "height": "47.2",
+    "min_height": "40.2",
+    "max_height": "49.2",
+    "age": 9,
+    "min_age": 7,
+    "max_age": 11,
+    "img": "https://s3.amazonaws.com/tunza-dev/dresses/images/000/000/041/original/18451715_1189482224494681_1552623127227096753_o.jpg?1505155167",
+    "order_dates": [
+      {
+        "start_date": "2017-05-03T18:39:29.113Z",
+        "end_date": "2017-05-17T18:39:29.113Z",
+        "id": 17
+      },
+      {
+        "start_date": "2017-05-30T18:39:29.113Z",
+        "end_date": "2017-06-13T18:39:29.113Z",
+        "id": 44
+      },
+      {
+        "start_date": "2017-06-21T18:39:29.113Z",
+        "end_date": "2017-07-05T18:39:29.113Z",
+        "id": 66
+      },
+      {
+        "start_date": "2017-07-20T18:39:29.113Z",
+        "end_date": "2017-08-03T18:39:29.113Z",
+        "id": 95
+      },
+      {
+        "start_date": "2017-08-13T18:39:29.113Z",
+        "end_date": "2017-08-27T18:39:29.113Z",
+        "id": 119
+      },
+      {
+        "start_date": "2017-08-30T18:39:29.113Z",
+        "end_date": "2017-09-13T18:39:29.113Z",
+        "id": 136
+      },
+      {
+        "start_date": "2017-09-26T18:39:29.113Z",
+        "end_date": "2017-10-10T18:39:29.113Z",
+        "id": 163
+      },
+      {
+        "start_date": "2017-10-20T18:39:29.113Z",
+        "end_date": "2017-11-03T18:39:29.113Z",
+        "id": 187
+      }
+    ]
+  },
+  {
+    "id": 42,
+    "barcode": "6423196325582",
+    "title": "Aerodynamic Concrete Coat",
+    "color": "cyan",
+    "description": "Food truck scenester keffiyeh pug salvia. Kinfolk neutra asymmetrical kale chips occupy portland cornhole. Selfies authentic wes anderson occupy try-hard cornhole irony messenger bag. 90's vhs beard chicharrones meditation health locavore sustainable. Normcore blue bottle next level flannel organic.",
+    "waist": "35.5",
+    "min_waist": "34.0",
+    "max_waist": "37.0",
+    "sleeve_length": "half length",
+    "height": "58.1",
+    "min_height": "51.1",
+    "max_height": "60.1",
+    "age": 7,
+    "min_age": 5,
+    "max_age": 9,
+    "img": "https://s3.amazonaws.com/tunza-dev/dresses/images/000/000/042/original/18489729_1194776347298602_1318183542409804243_o.jpg?1505155167",
+    "order_dates": [
+      {
+        "start_date": "2017-05-03T18:39:29.113Z",
+        "end_date": "2017-05-17T18:39:29.113Z",
+        "id": 17
+      },
+      {
+        "start_date": "2017-05-31T18:39:29.113Z",
+        "end_date": "2017-06-14T18:39:29.113Z",
+        "id": 45
+      },
+      {
+        "start_date": "2017-06-22T18:39:29.113Z",
+        "end_date": "2017-07-06T18:39:29.113Z",
+        "id": 67
+      },
+      {
+        "start_date": "2017-07-21T18:39:29.113Z",
+        "end_date": "2017-08-04T18:39:29.113Z",
+        "id": 96
+      },
+      {
+        "start_date": "2017-08-13T18:39:29.113Z",
+        "end_date": "2017-08-27T18:39:29.113Z",
+        "id": 119
+      },
+      {
+        "start_date": "2017-08-31T18:39:29.113Z",
+        "end_date": "2017-09-14T18:39:29.113Z",
+        "id": 137
+      },
+      {
+        "start_date": "2017-09-27T18:39:29.113Z",
+        "end_date": "2017-10-11T18:39:29.113Z",
+        "id": 164
+      },
+      {
+        "start_date": "2017-10-20T18:39:29.113Z",
+        "end_date": "2017-11-03T18:39:29.113Z",
+        "id": 187
+      }
+    ]
+  },
+  {
+    "id": 43,
+    "barcode": "3347396965566",
+    "title": "Awesome Steel Hat",
+    "color": "cyan",
+    "description": "Yolo beard tilde blog wes anderson. Fingerstache roof polaroid vinyl cliche narwhal. Pour-over five dollar toast mlkshk schlitz marfa pitchfork. Leggings schlitz actually helvetica plaid.",
+    "waist": "37.7",
+    "min_waist": "36.2",
+    "max_waist": "39.2",
+    "sleeve_length": "half length",
+    "height": "41.6",
+    "min_height": "34.6",
+    "max_height": "43.6",
+    "age": 3,
+    "min_age": 1,
+    "max_age": 5,
+    "img": "https://s3.amazonaws.com/tunza-dev/dresses/images/000/000/043/original/20264881_1255942184515351_8019815516709047551_n.jpg?1505155168",
+    "order_dates": [
+      {
+        "start_date": "2017-05-04T18:39:29.113Z",
+        "end_date": "2017-05-18T18:39:29.113Z",
+        "id": 18
+      },
+      {
+        "start_date": "2017-05-31T18:39:29.113Z",
+        "end_date": "2017-06-14T18:39:29.113Z",
+        "id": 45
+      },
+      {
+        "start_date": "2017-06-23T18:39:29.113Z",
+        "end_date": "2017-07-07T18:39:29.113Z",
+        "id": 68
+      },
+      {
+        "start_date": "2017-07-21T18:39:29.113Z",
+        "end_date": "2017-08-04T18:39:29.113Z",
+        "id": 96
+      },
+      {
+        "start_date": "2017-08-13T18:39:29.113Z",
+        "end_date": "2017-08-27T18:39:29.113Z",
+        "id": 119
+      },
+      {
+        "start_date": "2017-09-01T18:39:29.113Z",
+        "end_date": "2017-09-15T18:39:29.113Z",
+        "id": 138
+      },
+      {
+        "start_date": "2017-09-28T18:39:29.113Z",
+        "end_date": "2017-10-12T18:39:29.113Z",
+        "id": 165
+      },
+      {
+        "start_date": "2017-10-20T18:39:29.113Z",
+        "end_date": "2017-11-03T18:39:29.113Z",
+        "id": 187
+      }
+    ]
+  },
+  {
+    "id": 44,
+    "barcode": "2632449818212",
+    "title": "Incredible Paper Chair",
+    "color": "blue",
+    "description": "Sustainable beard diy fashion axe. Organic +1 skateboard kitsch pop-up loko pinterest. Farm-to-table waistcoat heirloom tousled keytar.",
+    "waist": "36.4",
+    "min_waist": "34.9",
+    "max_waist": "37.9",
+    "sleeve_length": "3 / 4",
+    "height": "69.0",
+    "min_height": "62.0",
+    "max_height": "71.0",
+    "age": 3,
+    "min_age": 1,
+    "max_age": 5,
+    "img": "https://s3.amazonaws.com/tunza-dev/dresses/images/000/000/044/original/20286862_1255942184515351_8019815516709047551_o.jpg?1505155168",
+    "order_dates": [
+      {
+        "start_date": "2017-05-05T18:39:29.113Z",
+        "end_date": "2017-05-19T18:39:29.113Z",
+        "id": 19
+      },
+      {
+        "start_date": "2017-05-31T18:39:29.113Z",
+        "end_date": "2017-06-14T18:39:29.113Z",
+        "id": 45
+      },
+      {
+        "start_date": "2017-06-24T18:39:29.113Z",
+        "end_date": "2017-07-08T18:39:29.113Z",
+        "id": 69
+      },
+      {
+        "start_date": "2017-07-21T18:39:29.113Z",
+        "end_date": "2017-08-04T18:39:29.113Z",
+        "id": 96
+      },
+      {
+        "start_date": "2017-08-14T18:39:29.113Z",
+        "end_date": "2017-08-28T18:39:29.113Z",
+        "id": 120
+      },
+      {
+        "start_date": "2017-09-02T18:39:29.113Z",
+        "end_date": "2017-09-16T18:39:29.113Z",
+        "id": 139
+      },
+      {
+        "start_date": "2017-09-28T18:39:29.113Z",
+        "end_date": "2017-10-12T18:39:29.113Z",
+        "id": 165
+      },
+      {
+        "start_date": "2017-10-20T18:39:29.113Z",
+        "end_date": "2017-11-03T18:39:29.113Z",
+        "id": 187
+      }
+    ]
+  },
+  {
+    "id": 45,
+    "barcode": "1628380302904",
+    "title": "Incredible Granite Gloves",
+    "color": "lavender",
+    "description": "Wayfarers kale chips pork belly tousled viral waistcoat chillwave vhs. Small batch ethical kinfolk distillery tote bag vegan. Thundercats wolf schlitz pinterest polaroid literally chillwave.",
+    "waist": "45.9",
+    "min_waist": "44.4",
+    "max_waist": "47.4",
+    "sleeve_length": "Half Length",
+    "height": "64.5",
+    "min_height": "57.5",
+    "max_height": "66.5",
+    "age": 5,
+    "min_age": 3,
+    "max_age": 7,
+    "img": "https://s3.amazonaws.com/tunza-dev/dresses/images/000/000/045/original/20292752_1255940924515477_1329420521966820256_n_%281%29.jpg?1505155168",
+    "order_dates": [
+      {
+        "start_date": "2017-05-06T18:39:29.113Z",
+        "end_date": "2017-05-20T18:39:29.113Z",
+        "id": 20
+      },
+      {
+        "start_date": "2017-05-31T18:39:29.113Z",
+        "end_date": "2017-06-14T18:39:29.113Z",
+        "id": 45
+      },
+      {
+        "start_date": "2017-06-24T18:39:29.113Z",
+        "end_date": "2017-07-08T18:39:29.113Z",
+        "id": 69
+      },
+      {
+        "start_date": "2017-07-21T18:39:29.113Z",
+        "end_date": "2017-08-04T18:39:29.113Z",
+        "id": 96
+      },
+      {
+        "start_date": "2017-08-15T18:39:29.113Z",
+        "end_date": "2017-08-29T18:39:29.113Z",
+        "id": 121
+      },
+      {
+        "start_date": "2017-09-02T18:39:29.113Z",
+        "end_date": "2017-09-16T18:39:29.113Z",
+        "id": 139
+      },
+      {
+        "start_date": "2017-09-29T18:39:29.113Z",
+        "end_date": "2017-10-13T18:39:29.113Z",
+        "id": 166
+      },
+      {
+        "start_date": "2017-10-20T18:39:29.113Z",
+        "end_date": "2017-11-03T18:39:29.113Z",
+        "id": 187
+      }
+    ]
+  }
+];
+
+export default allDressData;
